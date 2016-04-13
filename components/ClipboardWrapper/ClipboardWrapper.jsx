b1453271cc64686b4ee7bc57723d59368e72e2c9
Ivan Sosnin <isosnin@skbkontur.ru>
2016-04-13 09:47:29 +0500

    Правки после ревью

--- components/ClipboardWrapper/ClipboardWrapper.jsx
+++ components/ClipboardWrapper/ClipboardWrapper.jsx
@@ -29,7 +29,6 @@ class ClipboardWrapper extends Component {
 
     componentDidMount() {
         this._resolveCopyToClipboard();
-        this._reinitClipboard();
     }
 
     componentDidUpdate() {
