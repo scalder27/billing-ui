5d2b43db59f8721fae64c8c6ad9fa991196aeb60
oksanchike <oksanchike@skbkontur.ru>
2016-05-25 18:01:37 +0500

    fix className у ссылки

--- components/Link/Link.jsx
+++ components/Link/Link.jsx
@@ -5,15 +5,17 @@ import styles from "./Link.scss";
 
 const Link = (props) => {
     const {href, children, className} = props;
+    const  classNames = classnames(className, styles.link);
+
     if (href) {
         return (
-            <a {...props}>
+            <a {...props} className={classNames}>
                 {children}
             </a>);
     }
 
     return (
-        <span {...props} className={classnames(className, styles.link)}>
+        <span {...props} className={classNames}>
             {children}
         </span>
     )
