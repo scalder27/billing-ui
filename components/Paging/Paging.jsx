49855ff15c72274fc0c4511c674928d8edbbbb75
Ivan Sosnin <isosnin@skbkontur.ru>
2016-05-31 14:42:23 +0500

    Отрефакторил Paging

--- components/Paging/Paging.jsx
+++ components/Paging/Paging.jsx
@@ -1,23 +1,24 @@
 import {Component, PropTypes } from "react";
 import classnames from "classnames";
-import "./Pagimg.scss";
+import { range } from "underscore";
 
-const edgeCount = 5;
+import pagingStyles from "./Paging.scss";
 
 class Paging extends Component {
     getPage(pageCount, counter) {
-        const { CurrentPage, isSmall, onChange } = this.props;
+        const { CurrentPage, isSmall, onChange, edgeCount } = this.props;
 
-        const paginationActiveClassNames = classnames("pagination-active", {
-            "small-pagination-active": isSmall
+        const paginationActiveClassNames = classnames(pagingStyles.active, {
+            [pagingStyles.small]: isSmall
         });
 
-        const paginationLinkClassNames = classnames("pagination-link base-link-twin", {
-            "small-pagination-link": isSmall
+        const paginationLinkClassNames = classnames(pagingStyles.link, {
+            [pagingStyles.small]: isSmall
         });
 
         const leftEdge = CurrentPage - edgeCount;
         const rightEdge = CurrentPage + edgeCount;
+
         if (counter !== 1 && counter !== pageCount && (leftEdge >= counter || rightEdge <= counter)) {
             return leftEdge === counter || rightEdge === counter ? <span key={`ellipsis_${counter}`}>...</span> : null;
         }
@@ -35,24 +36,24 @@ class Paging extends Component {
     }
 
     getPages() {
-        const { PageSize, Total} = this.props;
+        const { PageSize, Total } = this.props;
         const pageCount = Math.floor(Total / PageSize) + (Total % PageSize !== 0 ? 1 : 0);
-        return _.range(1, pageCount + 1).map(counter => this.getPage(pageCount, counter));
+        return range(1, pageCount + 1).map(counter => this.getPage(pageCount, counter));
     }
 
     render() {
-        const { PageSize, Total, isSmall } = this.props;
+        const { PageSize, Total, isSmall, wrapperClass } = this.props;
 
         if (Total <= PageSize) {
             return null;
         }
 
-        const paginationClassNames = classnames("pagination", {
-            "small-pagination": isSmall
+        const paginationClassNames = classnames(pagingStyles.pagination, wrapperClass, {
+            [pagingStyles.small]: isSmall
         });
 
-        const paginationRowClassNames = classnames("pagination-row", {
-            "small-pagination-row": isSmall
+        const paginationRowClassNames = classnames(pagingStyles.row, {
+            [pagingStyles.small]: isSmall
         });
 
         return (
@@ -70,7 +71,13 @@ Paging.propTypes = {
     CurrentPage: PropTypes.number.isRequired,
     PageSize: PropTypes.number.isRequired,
     Total: PropTypes.number.isRequired,
-    isSmall: PropTypes.bool.isRequired
+    isSmall: PropTypes.bool.isRequired,
+    edgeCount: PropTypes.number,
+    wrapperClass: PropTypes.string
+};
+
+Paging.defaultProps = {
+    edgeCount: 5
 };
 
 export default Paging;
