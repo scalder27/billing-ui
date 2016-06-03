6483acdeadbe2b1180781789e018cdb7b1de6daa
scalder <scalder27@gmail.com>
2016-06-03 14:35:28 +0500

    втащил генератор гуида. Надо будет потом порефакторить и тесты написать

---
+++ helpers/__test__/GuidFactory.js
@@ -0,0 +1,28 @@
+var GuidFactory = {
+    create: function () {
+        return [this._gen(2), this._gen(1), this._gen(1), this._gen(1), this._gen(3)].join("-");
+    },
+    createList: function (count) {
+        var guids = [];
+        for (var index = 0; index < count; index++) {
+            guids.push(this.create());
+        }
+
+        return guids;
+    },
+    _s4: function () {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    },
+    _gen: function(times) {
+        var result = "";
+        for (var i = 0; i < times; ++i) {
+            result += this._s4();
+        }
+        return result;
+    }
+};
+
+export const createGuid = () => GuidFactory.create();
+export const createGuidList = (count = 0) => GuidFactory.createList(count);
