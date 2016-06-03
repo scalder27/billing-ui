cb5d2a5fd0222707655f3475e6287421bb7c4cc6
Ivan Sosnin <isosnin@skbkontur.ru>
2016-06-03 15:59:57 +0500

    Добавил customVar в GoogleAnalytics

--- helpers/GoogleAnalytics.js
+++ helpers/GoogleAnalytics.js
@@ -4,6 +4,10 @@ class GoogleAnalytics {
     static triggerEventAsync(category, action, label) {
         GaEventHelper && GaEventHelper.publishEventAsync(category, action, label);
     }
+
+    static customVar(name, value) {
+        GaEventHelper && GaEventHelper.customVar(name, value);
+    }
 }
 
 export default GoogleAnalytics;
