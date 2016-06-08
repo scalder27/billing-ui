703306c349ee283e91321fa7b0331e7fdd57ee20
oksanchike <oksanchike@skbkontur.ru>
2016-06-08 11:53:14 +0500

    WebUI/Partners: Прокачала GuidFactory

--- helpers/GuidFactory.js
+++ helpers/GuidFactory.js
@@ -1,8 +1,10 @@
-var GuidFactory = {
-    create: function () {
+const nullGuid = "00000000-0000-0000-0000-000000000000";
+
+const GuidFactory = {
+    create() {
         return [this._gen(2), this._gen(1), this._gen(1), this._gen(1), this._gen(3)].join("-");
     },
-    createList: function (count) {
+    createList(count) {
         var guids = [];
         for (var index = 0; index < count; index++) {
             guids.push(this.create());
@@ -10,12 +12,15 @@ var GuidFactory = {
 
         return guids;
     },
-    _s4: function () {
+    isNullOrEmpty(guid) {
+        return !guid || guid === nullGuid
+    },
+    _s4() {
         return Math.floor((1 + Math.random()) * 0x10000)
             .toString(16)
             .substring(1);
     },
-    _gen: function(times) {
+    _gen(times) {
         var result = "";
         for (var i = 0; i < times; ++i) {
             result += this._s4();
@@ -25,4 +30,5 @@ var GuidFactory = {
 };
 
 export const createGuid = () => GuidFactory.create();
+export const isNullOrEmpty = (guid) => GuidFactory.isNullOrEmpty(guid);
 export const createGuidList = (count = 0) => GuidFactory.createList(count);
