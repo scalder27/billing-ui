57e59a0328249e76c02455e507ce7cb7e9b2ea59
oksanchike <oksanchike@skbkontur.ru>
2016-07-06 16:19:09 +0500

    fix NavigatorResolver

--- helpers/NavigatorResolver.js
+++ helpers/NavigatorResolver.js
@@ -7,6 +7,7 @@ const NavigatorResolver = {
     },
     isWinXP: navigator.userAgent.toLowerCase().indexOf("windows nt 5.1") > 0,
     isWin10: navigator.userAgent.toLowerCase().indexOf("windows nt 10.0") > 0,
+    isEdge: navigator.appVersion.indexOf("Edge") > 0,
     isIE11: navigator.appVersion.indexOf("rv:11.0") > 0,
     isIE10: navigator.appVersion.indexOf("MSIE 10.") !== -1,
     isIE: $("html").hasClass("ie"),
