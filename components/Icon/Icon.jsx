19c9d7ea43d22aca6ce0c097ddd51f89aea4836c
Осипов Андрей Олегович <andrayosipov@kontur>
2016-07-12 18:24:11 +0500

    Добавили ShouldComponentUpdate в Icon

--- components/Icon/Icon.jsx
+++ components/Icon/Icon.jsx
@@ -1,8 +1,11 @@
 import { Component, PropTypes } from "react";
 import IconType from "./IconType";
 import classnames from "classnames";
+import shouldComponentUpdate from "react-pure-render/function";
 
 class Icon extends Component {
+    shouldComponentUpdate = shouldComponentUpdate;
+
     render() {
         const {type, className} = this.props;
         const iconClass = classnames("iconic base-unselectable", className);
