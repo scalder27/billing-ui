c8d538dc341fac875446a9e97ebfc557055f3ffe
Ivan Sosnin <isosnin@skbkontur.ru>
2016-07-21 10:38:16 +0500

    Добавил keyCode backspace

--- helpers/KeyCodes.js
+++ helpers/KeyCodes.js
@@ -2,14 +2,19 @@ const KeyCodes = {
     tab: 9,
     enter: 13,
     esc: 27,
+    backspace: 8,
+
     left: 37,
     top: 38,
     right: 39,
     bottom: 40,
+
     home: 36,
     end: 35,
+
     pageUp: 33,
     pageDown: 34,
+
     g: 71,
     j: 74
 };
