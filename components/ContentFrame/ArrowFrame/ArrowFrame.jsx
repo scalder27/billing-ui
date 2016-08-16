4c1742eca8e963da6742a992c98c0a439821b9b2
Ivan Sosnin <isosnin@skbkontur.ru>
2016-08-16 17:44:01 +0500

    Сверстал ArrowFrame

--- components/ContentFrame/ArrowFrame/ArrowFrame.jsx
+++ components/ContentFrame/ArrowFrame/ArrowFrame.jsx
@@ -1,19 +1,36 @@
 import { Component, PropTypes } from "react";
-// import styles from "./ArrowFrame.scss";
+import Icon, { IconTypes } from "../../Icon";
+import arrowFrameStyles from "./ArrowFrame.scss";
 
 class ArrowFrame extends Component {
     render() {
+        const { closeUrl, backText, title, children } = this.props;
+
         return (
-            <div></div>
+            <div className={arrowFrameStyles.frame}>
+                <div className={arrowFrameStyles.header}>
+                    <a href={closeUrl} className={arrowFrameStyles.back}>
+                        <Icon type={IconTypes.ArrowChevronLeft} className={arrowFrameStyles.icon} />
+                        <div className={arrowFrameStyles["back-text"]}>{backText}</div>
+                    </a>
+                    <div className={arrowFrameStyles.title}>{title}</div>
+                </div>
+                <div className={arrowFrameStyles.content}>{children}</div>
+            </div>
         );
     }
 }
 
 ArrowFrame.propTypes = {
-    closeUrl: PropTypes.string,
-    title: PropTypes.oneOf([PropTypes.string, PropTypes.node]),
+    closeUrl: PropTypes.string.isRequired,
+    title: PropTypes.oneOf([PropTypes.string, PropTypes.node]).isRequired,
     backText: PropTypes.string,
+    styles: PropTypes.object,
     children: PropTypes.node
+};
+
+ArrowFrame.defaultProps = {
+    styles: arrowFrameStyles
 };
 
 export default ArrowFrame;
