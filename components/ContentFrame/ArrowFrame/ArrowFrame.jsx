6f34513271421eabeb99f58afa428ecbe50ca513
Ivan Sosnin <isosnin@skbkontur.ru>
2016-08-19 17:43:26 +0500

    Поменял ссылку в ArrowFrame на роутовую

--- components/ContentFrame/ArrowFrame/ArrowFrame.jsx
+++ components/ContentFrame/ArrowFrame/ArrowFrame.jsx
@@ -1,4 +1,5 @@
 import { PureComponent, PropTypes } from "react";
+import RouteLink from "react-router/lib/Link";
 import Icon, { IconTypes } from "../../Icon";
 import arrowFrameStyles from "./ArrowFrame.scss";
 
@@ -9,10 +10,12 @@ class ArrowFrame extends PureComponent {
         return (
             <div className={arrowFrameStyles.frame}>
                 <div className={arrowFrameStyles.header}>
-                    <a href={closeUrl} className={arrowFrameStyles.back}>
-                        <Icon type={IconTypes.ArrowChevronLeft} className={arrowFrameStyles.icon} />
-                        <div className={arrowFrameStyles["back-text"]}>{backText}</div>
-                    </a>
+                    <RouteLink to={closeUrl}>
+                        <div className={arrowFrameStyles.back}>
+                            <Icon type={IconTypes.ArrowChevronLeft} className={arrowFrameStyles.icon} />
+                            <div className={arrowFrameStyles["back-text"]}>{backText}</div>
+                        </div>
+                    </RouteLink>
                     <div className={arrowFrameStyles.title}>{title}</div>
                 </div>
                 <div className={arrowFrameStyles.content}>{children}</div>
