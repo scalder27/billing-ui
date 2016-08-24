f40226078169034b595d54c8d66f44977a83ee76
Осипов Андрей Олегович <andrayosipov@kontur>
2016-08-24 18:36:45 +0500

    Поправил передачу eventa в колбэк кнопки

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -19,7 +19,7 @@ class Button extends Component {
         const { disabled, onClick } = this.props;
 
         if (!disabled) {
-            onClick(arguments);
+            onClick(...arguments);
         }
     }
 
