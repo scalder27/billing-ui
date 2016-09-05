db70cffaa4febe5c5fb3aa7bca39e80397a41609
Ivan Sosnin <isosnin@skbkontur.ru>
2016-09-05 13:09:04 +0500

    Поправил onClick в Icon

--- components/Icon/Icon.jsx
+++ components/Icon/Icon.jsx
@@ -4,10 +4,10 @@ import classnames from "classnames";
 
 class Icon extends PureComponent {
     render() {
-        const {type, className} = this.props;
+        const { type, className, onClick } = this.props;
         const iconClass = classnames("iconic base-unselectable", className);
         return (
-            <span className={iconClass} unselectable="on">
+            <span className={iconClass} unselectable="on" onClick={onClick}>
                 {type}
             </span>
         );
@@ -16,11 +16,13 @@ class Icon extends PureComponent {
 
 Icon.propTypes = {
     type: PropTypes.string.isRequired,
-    className: PropTypes.string.isRequired
+    className: PropTypes.string.isRequired,
+    onClick: PropTypes.func
 };
 
 Icon.defaultProps = {
-    className: ""
+    className: "",
+    onClick: () => {}
 };
 
 export const IconTypes = IconType;
