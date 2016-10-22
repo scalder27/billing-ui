aa46731c3d57126ea2cfede7314899d61783a4f3
oksanchike <oksanchike@skbkontur.ru>
2016-10-22 13:28:05 +0500

    TextInput: экспорт типа тултипа

--- components/TextInput/index.js
+++ components/TextInput/index.js
@@ -1,6 +1,7 @@
 import Validation from "../../helpers/ValidationHelpers";
 import TextInputType from "./TextInputType";
 import TextInputWrapper from "./TextInputWrapper.jsx";
+import { TooltipTypes, PositionTypes } from "../Tooltip";
 
-export { TextInputType, TextInputWrapper, Validation };
+export { TextInputType, TextInputWrapper, Validation, PositionTypes as TooltipPositionTypes, TooltipTypes };
 export default TextInputWrapper;
